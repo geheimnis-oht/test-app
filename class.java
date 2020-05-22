72ecfe1f5607bbce372feb80c644f85194928b1b
Mohamed LAIDANI <laidani.it@gmail.com>
2020-05-22 18:39:02 +0100

    class and employee file modified

---
+++ class.java
@@ -0,0 +1,6 @@
+class MyClass {
+  private String username;
+  private String description;
+  private LocalDate dateOfBirth;
+  private long height;  
+}
---
+++ employee.java
@@ -0,0 +1,12 @@
+class Employee {
+  private String empName;
+  private String empDescription;
+  private LocalDate dateOfBirth;
+  private long weight; 
+
+  public String getEmpName() {
+    return this.empName;
+  } 
+
+  
+}
